6b4db88de261cd4f8f6202cf5bb28a81b9f3053f
Luke <lmcdougall@unm.edu>
2023-05-06 18:02:51 -0600

    refactored classes into their own packages

---
+++ src/Bank/Bank.java
@@ -0,0 +1,13 @@
+package Bank;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Bank {
+
+    private List<AuctionHouse> auctionHouses;
+    private HashMap<Integer, Account> accounts;
+    private HashMap<Integer, Double> blockedMoney;
+    private final double BALANCE = 9999999;
+
+}
