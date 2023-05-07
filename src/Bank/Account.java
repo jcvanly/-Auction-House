5ed6f57f6d0ab6289909e7fa85e8ce15768a0c45
Luke <lmcdougall@unm.edu>
2023-05-06 18:10:04 -0600

    Added basic bank account functionality in Account.java

--- src/Bank/Account.java
+++ src/Bank/Account.java
@@ -1,4 +1,62 @@
 package Bank;
 
 public class Account {
+    private final int accountNumber;
+    private String accountName;
+    private double balance;
+
+    public Account(int accountNumber,double balance) {
+        this.accountNumber = accountNumber;
+        accountName = "";
+        this.balance = balance;
+    }
+
+    /**
+     * Sets name.
+     * @param name of type String
+     */
+    public void setName(String name) {
+        this.accountName = name;
+    }
+
+    /**
+     * Gets acct number
+     * @return the account number
+     */
+    public int getAcctNum() {
+        return accountNumber;
+    }
+
+    /**
+     * Gets the balance
+     * @return balance
+     */
+    public double getBal() {
+        return balance;
+    }
+
+    /**
+     * Gets name
+     * @return name
+     */
+    public String getName() {
+        return accountName;
+    }
+
+    /**
+     * Deducts funds when a bid is made.
+     * @param amount of type double
+     */
+    public void deductFunds(double amount){
+        this.balance -= amount;
+    }
+
+
+    /**
+     * Adds funds to the account for an outbid or win action.
+     * @param amount
+     */
+    public void addFunds(double amount){
+        this.balance += amount;
+    }
 }
--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,13 +1,21 @@
 package Bank;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import AuctionHouse.AuctionHouse;
+
 public class Bank {
 
-    private List<AuctionHouse> auctionHouses;
-    private HashMap<Integer, Account> accounts;
-    private HashMap<Integer, Double> blockedMoney;
-    private final double BALANCE = 9999999;
+    private final List<AuctionHouse> auctionHouses;
+    private final HashMap<Integer, Account> accounts;
+    private final HashMap<Integer, Double> blockedMoney;
+
+    public Bank() {
+        accounts = new HashMap<>();
+        auctionHouses = new ArrayList<>();
+        blockedMoney = new HashMap<>();
+    }
 
 }
