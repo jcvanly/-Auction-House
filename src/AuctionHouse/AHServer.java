fe664008d0bc502dbfb5612760655aed76514ca7
Luke <lmcdougall@unm.edu>
2023-05-07 19:24:26 -0600

    Added minor connection code for AHServer

--- src/AuctionHouse/AHServer.java
+++ src/AuctionHouse/AHServer.java
@@ -1,6 +1,8 @@
 package AuctionHouse;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,7 +14,10 @@ public class AHServer {
     private final String ahIPAddress;
     private final int ahPort;
 
-    public AHServer(String bankIPAddress, String ahIPAddress, int ahPort) throws IOException {
+    private ObjectOutputStream bankOutputStream;
+    private ObjectInputStream bankInputStream;
+
+    public AHServer(String bankIPAddress, String ahIPAddress, int ahPort) throws IOException, InterruptedException {
         this.bankIPAddress = bankIPAddress;
         this.ahIPAddress = ahIPAddress;
         this.ahPort = ahPort;
@@ -20,9 +25,11 @@ public class AHServer {
         ahServerSocket = new ServerSocket(ahPort);
         auctionHouse = new AuctionHouse();
 
+        connectToBankServer();
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter the IP ADDRESS of the Bank:");
         String bankIP = scnr.nextLine();
@@ -33,11 +40,31 @@ public class AHServer {
         new AHServer(bankIP, ahIP, ahPort);
     }
 
-    private Socket connectTo(String ip, int port) {
-        return null;
+    private Socket connectTo(String ip, int port) throws InterruptedException {
+        Socket socket = null;
+        boolean connected = false;
+        while (!connected) {
+            try {
+                socket = new Socket(ip, port);
+                connected = true;
+                System.out.println("Connection successful");
+            } catch (IOException e) {
+                System.out.println("Connection failed, trying again...");
+                Thread.sleep(1000);
+            }
+        }
+        return socket;
     }
 
 
+    private void connectToBankServer() throws InterruptedException, IOException {
+        Socket bankSocket = connectTo(bankIPAddress, 55555);
+        System.out.println("Connection established with bank @ " + bankSocket.getInetAddress() + ":" + bankSocket.getPort());
+
+        bankOutputStream = new ObjectOutputStream(bankSocket.getOutputStream());
+        bankInputStream = new ObjectInputStream(bankSocket.getInputStream());
+
+    }
 
 
 
