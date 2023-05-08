0bb319af2db6f23cb6bb39e773283fcc1d90de61
Jack Vanlyssel <jvanlyssel@unm.edu>
2023-05-08 17:29:32 -0600

    working on bid class

--- src/Agent/Bid.java
+++ src/Agent/Bid.java
@@ -1,4 +1,32 @@
 package Agent;
 
-public class Bid {
+import java.io.Serializable;
+
+public class Bid implements Serializable {
+    private final int agentID;
+    private final int itemID;
+    private final double amount;
+
+    public Bid(int agentID, int itemID, double amount){
+        this.agentID = agentID;
+        this.itemID = itemID;
+        this.amount = amount;
+    }
+
+    /**
+     * Get agent ID
+     * @return agent's ID
+     */
+    public int getAgentID() {
+        return agentID;
+    }
+
+
+    /**
+     * Get amount
+     * @return amount
+     */
+    public double getAmount() {
+        return amount;
+    }
 }
--- src/Agent/Client.java
+++ src/Agent/Client.java
@@ -1,4 +1,5 @@
 package Agent;
 
 public class Client {
+
 }
