b9e91dc4dc936dcaf727b6df5fc5c79a011e673e
Jack Vanlyssel <jvanlyssel@unm.edu>
2023-05-08 16:44:12 -0600

    updating agent with helper functions

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,5 +1,27 @@
 package Agent;
 
+import AuctionHouse.AuctionHouse;
+
+import java.util.List;
+
 public class Agent {
-//hii
+
+    private List<AuctionHouse> availableHouses;
+    private int accountNumber;
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAvailableHouses(List<AuctionHouse> availableHouses) {
+        this.availableHouses = availableHouses;
+    }
+
+    public List<AuctionHouse> getAvailableHouses() {
+        return availableHouses;
+    }
 }
