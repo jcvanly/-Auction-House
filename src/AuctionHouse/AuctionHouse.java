973190810919b3b21872fd4307de32658750032d
Jack Vanlyssel <jvanlyssel@unm.edu>
2023-05-09 20:45:10 -0600

    updating the client and auction house

--- src/AuctionHouse/AuctionHouse.java
+++ src/AuctionHouse/AuctionHouse.java
@@ -1,5 +1,183 @@
 package AuctionHouse;
 
-public class AuctionHouse {
+import Messages.BidMessage;
 
+import java.io.*;
+import java.util.*;
+
+public class AuctionHouse implements Serializable {
+    private final List<Item> items;
+    private final List<Item> itemsToSell;
+    private int auctionID = 0;
+    private final String ip;
+    private final int port;
+    private final boolean alive;
+    private int currItemId;
+    private final Map<Item, AuctionClient> bidMap;
+    private final Map<Integer, Item> accountHistory;
+    private final int winTime = 10;
+    private final double BID_OFFSET = 1000;
+
+    //List of states of each agent item
+
+    /**
+     * Constructor initializing an Auction House.
+     */
+    public AuctionHouse(String ip, int port, String file){
+        bidMap = new HashMap<>();
+        accountHistory = new HashMap<>();
+        alive = true;
+        this.ip = ip;
+        this.port = port;
+        this.items  = new ArrayList<>();
+        this.itemsToSell = new ArrayList<>();
+        this.currItemId = 0;
+        loadItems(file);
+
+        //generating 3 items to test if it works.
+
+        generateItem();
+        generateItem();
+        generateItem();
+        counter();
+    }
+
+    /**
+     * Counter
+     */
+    private void counter(){
+        Thread count = new Thread(()->{
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                    ArrayList<Item> itemListCopy=new ArrayList<>(itemsToSell);
+                    for(Item i : itemListCopy){
+                        i.increment();
+                        if(i.getItemCount() >= winTime &&
+                                i.getCurrBid() != 0){
+                            //Do win bid stuff
+                            if(!i.isWon()){
+                                bidMap.get(i).winBid(i);
+                                i.setWon(true);
+                                System.out.println(i.getCurrBid());
+                            }
+                        }
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        count.start();
+    }
+
+    /**
+     * Get items to be sold
+     * @return Items to be sold
+     */
+    public List<Item> getItems(){
+        System.out.println("Got items");
+        return itemsToSell;
+    }
+
+    /**
+     * Get port number
+     * @return port number
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * Get IP number
+     * @return IP address
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * Set auction ID
+     * @param auctionID action house ID of type int
+     */
+    public void setAuctionID(int auctionID) {
+        this.auctionID = auctionID;
+    }
+
+    /**
+     * Get auction ID
+     * @return auction ID
+     */
+    public int getAuctionID() {
+        return auctionID;
+    }
+
+    /**
+     * Bid
+     * @param client client
+     * @param bid bid message
+     * @return true if bid successfully
+     */
+    public boolean bid(AuctionClient client, BidMessage bid){
+        for(Item i: itemsToSell){
+            if(i.getItemID() == bid.getItem().getItemID()){
+                if(Double.compare( bid.getBid() , i.getMinBid())>= 0){
+                    if (accountHistory.containsKey(bid.getAcctNum())) {
+                        if(bid.getItem().getItemID() == i.getItemID()){
+                            System.out.println("Bid already made");
+                            return false;
+                        }
+                    }
+                    if (bidMap.containsKey(i)) {
+                        bidMap.get(i).outBid(i);
+                    }
+                    bidMap.put(i, client);
+                    i.setCurrBid(bid.getBid());
+                    i.setMinBid(i.getCurrBid() + BID_OFFSET);
+                    i.resetTimer();
+                    System.out.println("Successful bid on " +
+                            i.getName() + " for " + bid.getBid());
+                    System.out.println(i.getCurrBid());
+                    return true;
+                }
+            }
+        }
+        System.out.println("Unsuccessful bid on " + bid.getItem().getName());
+        return false;
+    }
+
+    /**
+     * loadItems reads in the items from items.txt file.
+     * @param file file of items of type String
+     */
+    private void loadItems(String file){
+        try {
+            BufferedReader bReader = new BufferedReader(new FileReader(file));
+            String line;
+
+            // Read in the items
+            while ((line = bReader.readLine()) != null) {
+                String[] x = line.split(",");
+                Item item = new Item(x[0],
+                        Integer.parseInt(x[1]), x[2], 0, 0);
+                items.add(item);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Picking random items from the items to sell list.
+     */
+    public void generateItem(){
+        Random ran = new Random();
+        Item i = items.get(ran.nextInt(items.size()));
+        itemsToSell.add(new Item(i.getName(), i.getMinBid(),
+                i.getDescription(), currItemId++, auctionID));
+    }
 }
+
--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -7,15 +7,166 @@ import java.util.List;
 import AuctionHouse.AuctionHouse;
 
 public class Bank {
-
     private final List<AuctionHouse> auctionHouses;
     private final HashMap<Integer, Account> accounts;
     private final HashMap<Integer, Double> blockedMoney;
+    private final double BALANCE = 9999999;
+    private final int MAX_SIZE = 10;
+    private final int portNumber;
+    private final int bankPort;
+    private final int currID = 0;
 
+    /**
+     * Instantiates a new Bank.
+     */
     public Bank() {
-        accounts = new HashMap<>();
-        auctionHouses = new ArrayList<>();
-        blockedMoney = new HashMap<>();
+        this.portNumber = portNumber;
+        this.bankPort = portNumber;
+        this.accounts = new HashMap<>();
+        this.auctionHouses = new ArrayList<>();
+        this.blockedMoney = new HashMap<>();
+    }
+
+    /**
+     * Register ah account.
+     * @param house the house
+     * @return the account
+     */
+    public synchronized Account registerAH(AuctionHouse house) {
+        int uniqueID = accounts.size() + 1;
+        Account acct = new Account(accounts.size() + 1, 0);
+        accounts.put(accounts.size() + 1, acct);
+        System.out.println("Registered Auction House account " +
+                "with ID of " + uniqueID);
+        house.setAuctionID(uniqueID);
+        auctionHouses.add(house);
+        return acct;
+    }
+
+
+    /**
+     * Register agent account.
+     * @param accountName of type String
+     * @return account
+     */
+    public Account registerAgent(String accountName) {
+        int uniqueID = accounts.size() + 1;
+        Account acct = new Account(uniqueID, BALANCE);
+        acct.setName(accountName);
+        accounts.put(uniqueID, acct);
+        return acct;
+    }
+
+    /**
+     * Contains auction house boolean.
+     * @param id of type int
+     * @return the boolean
+     */
+    public boolean containsAuctionHouse(int id){
+        for(AuctionHouse auction: auctionHouses){
+            if(auction.getAuctionID() == id ) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Remove auction house.
+     * @param id of tyoe int
+     */
+    public void removeAuctionHouse(int id){
+        ArrayList<AuctionHouse> houseCopy = new ArrayList<>(auctionHouses);
+        for (int i = 0; i < houseCopy.size(); i++) {
+            if(houseCopy.get(i).getAuctionID() == id) {
+                auctionHouses.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Gets account.
+     * @param id of type int
+     * @return the account
+     */
+    public Account getAccount(int id) {
+        return this.accounts.get(id);
+    }
+
+    /**
+     * Gets auction houses.
+     * @return the auction houses
+     */
+    public List<AuctionHouse> getAuctionHouses() {
+        return auctionHouses;
+    }
+
+    /**
+     * Hold action. Takes money from agent based off ID
+     * and puts it into the blockedMoney hashmap
+     * @param uID the u id of type Integer
+     * @param amt the amt of type double
+     */
+    public void holdAction(Integer uID, double amt) {
+        Account acct = accounts.get(uID);
+        double total =amt;
+        if (blockedMoney.containsKey(uID)){
+            total += blockedMoney.get(uID);
+        }
+        acct.deductFunds(total);
+        blockedMoney.put(uID, total);
+    }
+
+    /**
+     * Outbid action. Removes the funds from blockedMoney Hashmap
+     * based off ID and adds it to the
+     *agent account again based off their ID
+     * @param uID the u id of type int
+     */
+    public void outbidAction(int uID) {
+        Account acct = accounts.get(uID);
+        acct.addFunds(blockedMoney.get(uID));
+        blockedMoney.remove(uID);
+    }
+
+    /**
+     * Win action. Adds the funds to auction house account from respective bid
+     * @param agentID of type int
+     * @param auctionHouseID of type int
+     */
+    public void winAction(int agentID, int auctionHouseID) {
+        Account auctionAcct = accounts.get(auctionHouseID);
+        auctionAcct.addFunds(blockedMoney.get(agentID));
+        blockedMoney.remove(agentID);
+    }
+
+    /**
+     * Gets accounts.
+     * @return the accounts
+     */
+    public HashMap<Integer, Account> getAccounts() {
+        return accounts;
+    }
+
+
+
+    /**
+     * Sufficient funds boolean. Returns false if bid amount is greater
+     * than the available funds in the agent bank account. Returns true
+     * if bid amount is less than available funds in the bank account.
+     * @param id of type int
+     * @param amount of type double
+     * @return the boolean
+     */
+    public boolean sufficientFunds(int id, double amount) {
+
+        double requiredAmount = amount;
+        System.out.println(id);
+        if(blockedMoney.containsKey(id)) {
+            double holdOnMoney = blockedMoney.get(id);
+            requiredAmount =   amount + holdOnMoney;
+        }
+        System.out.println("Input     +++ checking");
+        System.out.println(amount + "   " + accounts.get(id).getBal());
+        return Double.compare(requiredAmount,accounts.get(id).getBal()) <= 0;
     }
 
 }
